48f644ec28bece33ac2681599f74aa1a7fa549ed
Hamoudibwaby <147352169+Hamoudibwaby@users.noreply.github.com>
2024-05-29 08:04:59 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -35,7 +35,7 @@ global.premium = ["237675551808"] // Premium User
 global.hituet = 0
 
 //bot sett
-global.typemenu = 'v8' // menu type 'v1' => 'v8'
+global.typemenu = 'v1' // menu type 'v1' => 'v8'
 global.typereply = 'v2' // reply type 'v1' => 'v3'
 global.autoblocknumber = '92' //set autoblock country code
 global.antiforeignnumber = '91' //set anti foreign number country code
